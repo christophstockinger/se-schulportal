20c3f26e050fc8b5cbf44c929d463b2ffdab8055
JPeter92 <34061312+JPeter92@users.noreply.github.com>
2018-01-25 20:49:40 +0100

    Update Test

--- test/Kalender/TermineTest.java
+++ test/Kalender/TermineTest.java
@@ -38,138 +38,17 @@ public class TermineTest {
     }
 
     /**
-     * Test of setDBdate method, of class Termine.
-     */
-    @Test
-    public void testSetDBdate() {
-        System.out.println("setDBdate");
-        String dbdate = "";
-        Termine instance = new Termine();
-        instance.setDBdate(dbdate);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getDBdate method, of class Termine.
-     */
-    @Test
-    public void testGetDBdate() {
-        System.out.println("getDBdate");
-        Termine instance = new Termine();
-        String expResult = "";
-        String result = instance.getDBdate();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of setDBzevo method, of class Termine.
-     */
-    @Test
-    public void testSetDBzevo() {
-        System.out.println("setDBzevo");
-        String dbzevo = "";
-        Termine instance = new Termine();
-        instance.setDBzevo(dbzevo);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getDBzevo method, of class Termine.
-     */
-    @Test
-    public void testGetDBzevo() {
-        System.out.println("getDBzevo");
-        Termine instance = new Termine();
-        String expResult = "";
-        String result = instance.getDBzevo();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of setDBzebi method, of class Termine.
-     */
-    @Test
-    public void testSetDBzebi() {
-        System.out.println("setDBzebi");
-        String dbzebi = "";
-        Termine instance = new Termine();
-        instance.setDBzebi(dbzebi);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getDBzebi method, of class Termine.
-     */
-    @Test
-    public void testGetDBzebi() {
-        System.out.println("getDBzebi");
-        Termine instance = new Termine();
-        String expResult = "";
-        String result = instance.getDBzebi();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of setDBbez method, of class Termine.
-     */
-    @Test
-    public void testSetDBbez() {
-        System.out.println("setDBbez");
-        String dbbez = "";
-        Termine instance = new Termine();
-        instance.setDBbez(dbbez);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getDBbez method, of class Termine.
-     */
-    @Test
-    public void testGetDBbez() {
-        System.out.println("getDBbez");
-        Termine instance = new Termine();
-        String expResult = "";
-        String result = instance.getDBbez();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
      * Test of getTermine method, of class Termine.
      */
     @Test
     public void testGetTermine() throws Exception {
         System.out.println("getTermine");
-        String expResult = "";
+        String expResult = "<div class='col-12 EinTermin'>" + alleTermine.get(1) + "</div>";
         String result = Termine.getTermine();
         assertEquals(expResult, result);
         // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        //fail("The test case is a prototype.");
     }
 
-    /**
-     * Test of getSubNavigation method, of class Termine.
-     */
-    @Test
-    public void testGetSubNavigation() {
-        System.out.println("getSubNavigation");
-        Termine instance = new Termine();
-        String expResult = "";
-        String result = instance.getSubNavigation();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
     
 }
